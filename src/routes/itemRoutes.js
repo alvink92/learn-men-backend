603f072c52adef48c5336eac6beb0b97fa9edaab
Alvin Kong <alvink92@gmail.com>
2017-11-11 14:26:49 -0800

    added additem route + view

--- src/routes/itemRoutes.js
+++ src/routes/itemRoutes.js
@@ -12,4 +12,8 @@ itemRouter.route("/single").get(function(req, res) {
   res.render("singleItem");
 });
 
+itemRouter.route("/add").get(function(req, res) {
+  res.render("addItem");
+});
+
 module.exports = itemRouter;
