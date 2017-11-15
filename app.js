a60594f1d95064c98cc0fdc203fd36512c5783ce
Alvin Kong <alvink92@gmail.com>
2017-11-15 14:02:30 -0800

    setup mongodb items table, finished post item end points

--- app.js
+++ app.js
@@ -2,13 +2,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const itemRouter = require("./src/routes/itemRoutes");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
 
+mongoose.connect("mongodb://localhost:27017/todos")    
+.then(() => {
+console.log('Connected');
+})
+.catch(err => {
+  console.error('App starting error:', err.stack);
+  process.exit(1);
+});
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use("/items", itemRouter);
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.listen(port, function() {
   console.log("Server is running on port:", port);
--- src/routes/itemRoutes.js
+++ src/routes/itemRoutes.js
@@ -1,11 +1,22 @@
 // itemRoutes.js
 
+const Item = require('../models/Item');
+
 const express = require("express");
 const app = express();
 const itemRouter = express.Router();
 
 itemRouter.route("/").get(function(req, res) {
-  res.render("items");
+  Item.find(function (err, itms) {
+    if(err) {
+      console.log(err);
+    } else {
+      for(var i=0; i < itms.length; i++) {
+        console.log(itms[i].item);
+      }
+      res.render('items', {itms: itms});
+    }
+  });
 });
 
 itemRouter.route("/single").get(function(req, res) {
@@ -14,6 +25,17 @@ itemRouter.route("/single").get(function(req, res) {
 
 itemRouter.route("/add").get(function(req, res) {
   res.render("addItem");
+});
+
+itemRouter.route('/add/post').post(function (req, res) {
+  var item = new Item(req.body);
+      item.save()
+    .then(item => {
+    res.redirect('/items');
+    })
+    .catch(err => {
+    res.status(400).send("unable to save to database");
+    });
 });
 
 module.exports = itemRouter;
